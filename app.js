90798b61d12272c26b880ec53b2ad9ec1e1c0e81
Mateusz Pokrywka <mateusz.pokrywka04@gmail.com>
2025-11-12 09:05:39 +0000

    Com3_3.2

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 
 const fetchButton = document.getElementById('getRandomGifButton');
+const SearchButton = document.getElementById('getSearchGifButton');
+const gifName = document.getElementById('gif-input');
 const gifContainer = document.getElementById('gifContainer');
 const errorContainer = document.getElementById('errorContainer');
 
 const API_KEY ='l8t9EivkrWkL61e3YVgC2MslC6FITWAh'; //Klucz kolegi, ponieważ na moim koncie nie działa
 const API_URL = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=g`;
+const SEARCH_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${gifName}&limit=1`;
 
 async function fetchRandomGif() {
     
@@ -36,6 +39,48 @@ async function fetchRandomGif() {
     }
 }
 
+async function fetchSearchGif() {
+
+    const query = gifName.value;
+
+    if (!query) {
+        displayError('Musisz wpisać frazę do wyszukania.');
+        return;
+    }
+
+    const SEARCH_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=1`;
+
+    gifContainer.innerHTML = '';
+    errorContainer.textContent = '';
+    gifContainer.textContent = 'Ładowanie...';
+
+    try {
+        const response = await fetch(SEARCH_URL);
+
+        if (!response.ok) {
+            throw new Error(`Błąd API: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        if (data.data.length === 0) {
+            throw new Error('Nie znaleziono GIF-ów dla tej frazy.');
+        }
+
+        const gifUrl = data.data[0].images.original.url;
+
+        if (!gifUrl) {
+             throw new Error('Nie znaleziono adresu URL GIF-a w odpowiedzi API.');
+        }
+
+        displayGif(gifUrl);
+
+    } catch (error) {
+        console.error('Wystąpił błąd:', error);
+        displayError(`Nie udało się załadować GIF-a. (${error.message})`);
+    }
+}
+
 function displayGif(url) {
     gifContainer.innerHTML = '';
     
@@ -52,3 +97,4 @@ function displayError(message) {
 }
 
 fetchButton.addEventListener('click', fetchRandomGif);
+SearchButton.addEventListener('click', fetchSearchGif);
